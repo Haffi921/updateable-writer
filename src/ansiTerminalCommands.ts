7cf783ff76dc2243cb3387169f84822f116951c7
Hafsteinn Ragnarsson <haffi921@gmail.com>
2024-08-11 17:29:16 +0200

    'as const' apparently useless in rollup?

--- src/ansiTerminalCommands.ts
+++ src/ansiTerminalCommands.ts
@@ -1,4 +1,4 @@
-const ANSI_ESCAPE = "\u001b" as const;
+const ANSI_ESCAPE = "\u001b";
 
 export const ANSI = {
   CURSOR: {
@@ -6,6 +6,6 @@ export const ANSI = {
     SHOW: `${ANSI_ESCAPE}[?25h`,
     UP: (lines = 1) => (lines > 0 ? `${ANSI_ESCAPE}[${lines}A` : ""),
     DOWN: (lines = 1) => (lines > 0 ? `${ANSI_ESCAPE}[${lines}B` : ""),
-  } as const,
+  },
   ERASELINE: `${ANSI_ESCAPE}[2K`,
 } as const;
